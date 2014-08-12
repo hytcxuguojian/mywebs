6e1725f43a545d5e5d48ba6559a09a2c1b4e0768
zixuwuyou <1075737942@qq.com>
2014-08-12 16:05:50 +0800

    Revert "我的前端作品集"
    This reverts commit d9f2a5a6d7b88f4447c715e5dcf8a48329b81aa3.

--- index/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(".ContentItem").hover(function(){
-		$(this).css("border","2px solid #389cff")
-		$(this).find(".ContentItemSpan").fadeOut();
-	},function(){
-		$(this).css("border","");
-		$(this).find(".ContentItemSpan").fadeIn();
-	});
-});
